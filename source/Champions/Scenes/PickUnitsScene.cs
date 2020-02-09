93bbac812b5bcfa24d5a38cca198a82d7b359f8a
nightblade9 <nightbladecodes@gmail.com>
2020-02-08 20:14:16 -0500

    Proof of concept for mouse overlap Fix: game wasn't compiled (not part of solution)

--- source/Champions/Scenes/PickUnitsScene.cs
+++ source/Champions/Scenes/PickUnitsScene.cs
@@ -1,5 +1,6 @@
 using Puffin.Core;
 using Puffin.Core.Ecs;
+using Puffin.Core.Ecs.Components;
 
 namespace DeenGames.Champions.Scenes
 {
@@ -7,7 +8,13 @@ namespace DeenGames.Champions.Scenes
     {
         public PickUnitsScene()
         {
-            this.Add(new Entity().Label("Hello from Puffin!"));
+            var e = new Entity().Label("");
+            this.Add(e);
+
+            this.Add(new Entity().Move(400, 300).Colour(0xFF0000, 64, 64)
+                .Overlap(64, 64, 0, 0, 
+                    () => e.Get<TextLabelComponent>().Text = "Mouse on",
+                    () => e.Get<TextLabelComponent>().Text = ""));
         }
     }
 }
