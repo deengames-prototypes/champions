603fa6c3bf3c0e13e0e79d5ca5148df61b064862
nightblade9 <nightbladecodes@gmail.com>
2020-02-14 20:23:39 -0500

    Art + specializations for monsters/bosses.

--- source/Champions/Models/Specialization.cs
+++ source/Champions/Models/Specialization.cs
@@ -8,5 +8,20 @@ namespace DeenGames.Champions.Models
 
         // Typically monsters
         Slime = 3,
+        Skeleton = 5,
+        Shade = 6,
+        Ent = 7,
+        Treeman = 8,
+        Sandworm = 10,
+        Goldwasp = 11,
+        Imp = 12,
+        Gorgon = 13,
+        Crystallis = 15,
+        Raider = 16,
+
+        // Bosses
+        Slugmoid = 4,
+        Redeyes = 9,
+        Toadzilla = 14
     }
 }
